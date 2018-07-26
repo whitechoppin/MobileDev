fcaf200f15980b3425df9e78f7cf98f469dd741e
Murat Baysangurov <v-mubays@microsoft.com>
2018-07-26 13:34:29 +0300

    Fix indentation

--- client/xamarin.forms/ZUMOAPPNAME/TodoList.xaml.cs
+++ client/xamarin.forms/ZUMOAPPNAME/TodoList.xaml.cs
@@ -13,7 +13,7 @@ namespace ZUMOAPPNAME
             InitializeComponent();
 
             manager = TodoItemManager.DefaultManager;
-			if (Device.RuntimePlatform == Device.Windows)
+            if (Device.RuntimePlatform == Device.Windows)
             {
                 var refreshButton = new Button
                 {
